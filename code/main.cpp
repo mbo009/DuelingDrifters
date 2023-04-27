f31fff269be2f009e6483cfde0e82631b59fb0e6
mciesla <miloszc009@gmail.com>
2023-04-27 19:27:29 +0200

    Added clock

--- code/main.cpp
+++ code/main.cpp
@@ -1,27 +1,38 @@
 #include "carSprite.hpp"
 #include <vector>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1024, 1024), "CARBRAWL");
     window.setFramerateLimit(60);
-    bool UpPressed, LeftPressed, DownPressed, RightPressed;
-    CarSprite car = CarSprite("Red", 300, 300);
-    CarSprite car2 = CarSprite("Red", 200, 200);
+
     sf::Texture mapTexture;
-    sf::SoundBuffer musicBuffer;
-    sf::Sound music;
-    musicBuffer.loadFromFile("music/battleTheme.ogg");
-    music.setBuffer(musicBuffer);
-    music.setLoop(true);
     mapTexture.loadFromFile("img/spaceMap.png");
     sf::Sprite map(mapTexture);
     map.setScale(sf::Vector2f(1.5, 1.5));
     map.setOrigin(190, 230);
-    car.setScale(sf::Vector2f(2.5, 2.5));
-    car2.setScale(sf::Vector2f(2.5, 2.5));
+
+    sf::SoundBuffer musicBuffer;
+    musicBuffer.loadFromFile("music/battleTheme.ogg");
+    sf::Sound music;
+    music.setBuffer(musicBuffer);
+    music.setLoop(true);
     music.play();
     music.setVolume(20);
+
+    sf::Font font;
+    font.loadFromFile("font/PixeloidMono.ttf");
+    sf::Text timerText("", font, 50);
+    timerText.setPosition(450.f, 50.f);
+    sf::Clock clock;
+    clock.restart();
+
+    CarSprite car = CarSprite("Red", 300, 300);
+    car.setScale(sf::Vector2f(2.5, 2.5));
+    CarSprite car2 = CarSprite("Red", 200, 200);
+    car2.setScale(sf::Vector2f(2.5, 2.5));
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -54,12 +65,18 @@ int main()
             else
                 car2.noMovementKeyPressed();
         }
+        sf::Time elapsed = clock.getElapsedTime();
+        int min = static_cast<int>(elapsed.asSeconds()) / 60;
+        int sec = static_cast<int>(elapsed.asSeconds()) % 60;
+        timerText.setString((min < 10 ? "0" + std::to_string(min) : std::to_string(min)) + ":" + (sec < 10 ? "0" + std::to_string(sec) : std::to_string(sec)));
+
         car.move();
         car2.move();
         window.clear(sf::Color::Black);
         window.draw(map);
         window.draw(car);
         window.draw(car2);
+        window.draw(timerText);
         window.display();
     }
 
