b4f9b47a904342c6852ae273043d26429a8a8208
Binh Vuong Le Duc <01184762@pw.edu.pl>
2023-04-28 22:59:17 +0200

    Implement checkCollision method

--- code/carSprite.cpp
+++ code/carSprite.cpp
@@ -10,25 +10,37 @@ public:
     }
 };
 
-
-CarSprite::CarSprite(const std::string &color, float x, float y) : x(x), y(y)
+CarSprite::CarSprite(const std::string &color, float x, float y, float scale) : x(x), y(y)
 {
-    this->setColor(color);
     this->x = x;
     this->y = y;
-    carObj = CarObj(x, y);
+    this->carObj = CarObj(x, y);
+    this->scale = scale;
+    this->setScale(sf::Vector2f(scale, scale));
+    this->setColor(color);
+    
 }
 
 void CarSprite::setColor(const std::string &color)
 {
     this->color = color;
-    textures.clear(); // Clear the textures vector
+    textures.clear();
     reloadTextures();
 }
 
 std::string CarSprite::getColor() const
 {
     return color;
+}
+
+int CarSprite::getKeyAction() const
+{
+    return keyAction;
+}
+
+int CarSprite::getScale() const
+{
+    return scale;
 }
 
 bool CarSprite::reloadTextures()
@@ -52,23 +64,23 @@ void CarSprite::updateDirectionTexture()
 
 void CarSprite::setNextAction(bool &UpPressed, bool &LeftPressed, bool &DownPressed, bool &RightPressed)
 {
-    if (UpPressed && RightPressed)              // Go north-east
+    if (UpPressed && RightPressed) // Go north-east
         this->keyAction = 1;
-    else if (RightPressed && DownPressed)       // Go south-east
+    else if (RightPressed && DownPressed) // Go south-east
         this->keyAction = 3;
-    else if (DownPressed && LeftPressed)        // Go south-west
+    else if (DownPressed && LeftPressed) // Go south-west
         this->keyAction = 5;
-    else if (LeftPressed && UpPressed)          // Go north-west
+    else if (LeftPressed && UpPressed) // Go north-west
         this->keyAction = 7;
-    else if (UpPressed)                         // Go north
+    else if (UpPressed) // Go north
         this->keyAction = 0;
-    else if (RightPressed)                      // Go east
+    else if (RightPressed) // Go east
         this->keyAction = 2;
-    else if (DownPressed)                       // Go south    
+    else if (DownPressed) // Go south
         this->keyAction = 4;
-    else if (LeftPressed)                       // Go west
+    else if (LeftPressed) // Go west
         this->keyAction = 6;
-} 
+}
 
 void CarSprite::noMovementKeyPressed()
 {
@@ -83,3 +95,44 @@ void CarSprite::move()
     setPosition(x, y);
 }
 
+bool CarSprite::checkCollision(const CarSprite &other)
+{
+    // Get the current texture for each sprite
+    const sf::Texture *texture1 = this->getTexture();
+    const sf::Texture *texture2 = other.getTexture();
+    if (!texture1 || !texture2) // Make sure both sprites have textures
+    {
+        return false;
+    }
+
+    // Get the global bounds of each sprite
+    sf::FloatRect bounds1 = this->getGlobalBounds();
+    sf::FloatRect bounds2 = other.getGlobalBounds();
+
+    // Calculate the intersection rectangle
+    sf::FloatRect intersection;
+    if (!bounds1.intersects(bounds2, intersection)) // No intersection, no collision
+    {
+        return false;
+    }
+
+    // Get the texture data for each sprite
+    sf::Image image1 = texture1->copyToImage();
+    sf::Image image2 = texture2->copyToImage();
+    // const sf::Uint8 *pixels1 = image1.getPixelsPtr();
+    // const sf::Uint8 *pixels2 = image2.getPixelsPtr();
+    // Check each pixel in the intersection area for opacity overlap
+    for (int x = intersection.left; x < intersection.left + intersection.width; x++)
+    {
+        for (int y = intersection.top; y < intersection.top + intersection.height; y++)
+        {
+            sf::Color color1 = image1.getPixel((x - bounds1.left)/this->scale, (y - bounds1.top)/this->scale);
+            sf::Color color2 = image2.getPixel((x - bounds2.left)/this->scale, (y - bounds2.top)/this->scale);
+            if (color1.a != 0 && color2.a != 0)
+            {
+                return true; // Collision detected
+            }
+        }
+    }
+    return false; // No collision detected
+}
--- code/carSprite.hpp
+++ code/carSprite.hpp
@@ -3,38 +3,41 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <memory>
 #include "carObj.hpp"
 
 class CarSprite : public sf::Sprite
 {
 public:
-    CarSprite(const std::string &color = "red", float x=0, float y=0);
-    void setColor(const std::string &newColor); // Set new color, reload textures
-    std::string getColor() const; // Return the color of the car
+    CarSprite(const std::string &color = "red", float x = 0, float y = 0, float scale = 1);
+    
+    void setColor(const std::string &newColor);
+    std::string getColor() const;
+    int getKeyAction() const;
+    int getScale() const;
+    
     bool reloadTextures();
     void updateDirectionTexture();
-    // TODO: Write setter for x, y
-    // Convert user input to parameters to pass to carObj
     void setNextAction(bool &WPressed, bool &APressed, bool &SPressed, bool &DPressed);
     void noMovementKeyPressed();
-    // Get updated position from carObj
     void move();
-    
+    bool checkCollision(const CarSprite &other);
+
 private:
-    //TODO: Add more colors, change the folder, change the extension
+    // TODO: Add more colors, change the folder, change the extension
     //=================THIS SHOULD BE CHANGE==============//
     const std::vector<std::string> files = {"up", "upRight", "right", "downRight",
-                                  "down", "downLeft", "left", "upLeft"};
+                                            "down", "downLeft", "left", "upLeft"};
     const std::string folder = "img";
     const std::string extension = "png";
     // ==================================================//
+    unsigned int keyAction = 8;
+    float scale = 1;
+    float x = 300;
+    float y = 300;
     std::vector<sf::Texture> textures;
     CarObj carObj;
     std::string color = "";
-    void changeVelocity(TriStateBool xAcc, TriStateBool yAcc);
-    unsigned int keyAction = 0;
-    float x = 300;
-    float y = 300;
 };
 
 #endif
