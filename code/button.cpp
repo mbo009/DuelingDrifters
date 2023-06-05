a1b82fcfc94406066eb2ea6d1a206cdc20248a55
bvuongle <ducbinhhn2002@gmail.com>
2023-06-06 00:58:18 +0200

    Refactor

--- code/button.cpp
+++ code/button.cpp
@@ -1,31 +1,10 @@
 #include "button.hpp"
 #include <map>
 
-std::map<std::string, std::string> baseTextures;
-std::map<std::string, std::string> highlightedTextures;
-
-void createBaseMap()
-{
-    baseTextures["start"] = ASSET_PATHS_HPP::START_BTN;
-    baseTextures["exit"] = ASSET_PATHS_HPP::EXIT_BTN;
-    baseTextures["duel"] = ASSET_PATHS_HPP::DUEL_BTN;
-    baseTextures["flag"] = ASSET_PATHS_HPP::FLAG_BTN;
-}
-
-void createHighlightedMap()
-{
-    highlightedTextures["start"] = ASSET_PATHS_HPP::HLT_START_BTN;
-    highlightedTextures["exit"] = ASSET_PATHS_HPP::HLT_EXIT_BTN;
-    highlightedTextures["duel"] = ASSET_PATHS_HPP::HLT_DUEL_BTN; // ::NORMAL;
-    highlightedTextures["flag"] = ASSET_PATHS_HPP::HLT_FLAG_BTN;   // ::TAG;
-}
-
 Button::Button(float x, float y, std::string name) : x(x), y(y), name(name)
 {
-    createBaseMap();
-    createHighlightedMap();
-    baseTexture.loadFromFile(baseTextures[name]);
-    highlightedTexture.loadFromFile(highlightedTextures[name]);
+    baseTexture.loadFromFile(texturePaths[name].first);
+    highlightedTexture.loadFromFile(texturePaths[name].second);
     setTexture(baseTexture);
     setPosition(x, y);
     setScale(2, 2);
--- code/button.hpp
+++ code/button.hpp
@@ -1,6 +1,8 @@
 #ifndef BUTTON_H
 #define BUTTON_H
+#pragma once
 
+#include <utility>
 #include "SFML/Graphics.hpp"
 #include "assetPaths.hpp"
 
@@ -13,6 +15,13 @@ public:
     std::string getName();
 
 private:
+    
+    std::map<std::string, std::pair<std::string, std::string>> texturePaths = {
+        {"start",{ASSET_PATHS_HPP::START_BTN, ASSET_PATHS_HPP::HLT_START_BTN}},
+        {"exit", {ASSET_PATHS_HPP::EXIT_BTN, ASSET_PATHS_HPP::HLT_EXIT_BTN}},
+        {"duel", {ASSET_PATHS_HPP::DUEL_BTN, ASSET_PATHS_HPP::HLT_DUEL_BTN}},
+        {"flag", {ASSET_PATHS_HPP::FLAG_BTN, ASSET_PATHS_HPP::HLT_FLAG_BTN}}
+    };
     sf::Texture baseTexture;
     sf::Texture highlightedTexture;
     bool highlighted = 0;
